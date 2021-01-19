0c6cf33bf55a8859a0ff77c48794636a1f616551
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2021-01-19 11:49:05 -0300

    Deploy website - based on d92d7442383318fd1adefa21b07e165921301d02

---
+++ 796bc4f9.9a74493f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{95:function(o){o.exports=JSON.parse('{"allTagsPath":"/rnds/blog/tags","slug":"comunicado","name":"comunicado","count":1,"permalink":"/rnds/blog/tags/comunicado"}')}}]);
