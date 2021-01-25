34ca9e1f297fc27d9c59d069ec33fe6f81434563
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2021-01-25 12:44:15 -0300

    Deploy website - based on df89ef840986b4efe3dc5ce16aa95ab44fa103cc

---
+++ 9e127427.a2ecdcb4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[48],{120:function(o){o.exports=JSON.parse('{"permalink":"/rnds/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
