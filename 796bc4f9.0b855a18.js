8ee0c7865cabad2dd394add07406bfb55bdcb35d
kyriosdata <fabio@inf.ufg.br>
2021-02-14 12:37:26 -0300

    Deploy website - based on 0466e1a7e2a02eb19b6e31010de15cf4f877336c

---
+++ 796bc4f9.0b855a18.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{104:function(o){o.exports=JSON.parse('{"allTagsPath":"/rnds/blog/tags","slug":"comunicado","name":"comunicado","count":1,"permalink":"/rnds/blog/tags/comunicado"}')}}]);
